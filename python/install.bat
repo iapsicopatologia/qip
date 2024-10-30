5984ca90b12d85a12c50c4ab50feb40bb520bbfa
josuemorais <josuemorais@gmail.com>
2024-10-30 13:03:27 -0300

    refactor python

---
+++ python/install.bat
@@ -0,0 +1,6 @@
+CALL python.exe -m pip install --upgrade pip
+CALL pip install openpyxl
+CALL pip install pandas 
+CALL pip install numpy 
+CALL pip install tensorflow
+CALL pip install scikit-learn
